0ed2f6103107b3e1d27c4d85316b169dcbffcdcb
k-daigo <k-daigo@lis.co.jp>
2021-01-03 22:21:41 +0900

    sineのプチプチ対応（してコメントアウト）

--- src/components/morse_player.js
+++ src/components/morse_player.js
@@ -47,7 +47,7 @@ export default class MorsePlayer {
       // 再生速度の設定
       playTime *= MORSE_SPEED;
 
-      gainList.push({ playTime, vol: 0.9 });
+      gainList.push({ playTime, vol: 1 });
 
       // 要素間のインターバル
       const interval = 1 * MORSE_SPEED;
@@ -68,22 +68,43 @@ export default class MorsePlayer {
       this.aurdioRunnable = true;
 
       this.oscillator = this.audioCtx.createOscillator();
-      this.oscillator.type = 'sine';
+      this.oscillator.type = 'square';
       this.oscillator.frequency.value = 440;
       const gain = this.audioCtx.createGain();
+      gain.gain.value = 0;
       gain.connect(this.audioCtx.destination);
 
-      this.oscillator.start();
       this.oscillator.connect(gain);
+      this.oscillator.start();
 
       // 指定要求があったら抜ける
       while (!this.audioReset) {
         let totalPlayTime = 0;
         const baseTime = this.audioCtx.currentTime;
+
+        // // 最初の音をフェードインさせるための音量設定（linearRampToValueAtTimeを利かせるために必要）
+        // gain.gain.setValueAtTime(0, baseTime);
+
         for (const gainItem of gainList) {
-          gain.gain.setValueAtTime(gainItem.vol, baseTime + totalPlayTime);
+          // sineで音の開始と終わりにプチプチしたノイズが発生する為、フェードインで滑らかにする
+          // 「0.001（1ms）」で固定的な時間でフェードインさせる
+          // gain.gain.linearRampToValueAtTime(
+          //   gainItem.vol,
+          //   baseTime + totalPlayTime + 0.001
+          // );
+
+          gain.gain.setValueAtTime(
+            gainItem.vol,
+            baseTime + totalPlayTime + 0.001
+          );
 
           totalPlayTime += gainItem.playTime / 1000;
+
+          // フェードアウト
+          // gain.gain.linearRampToValueAtTime(
+          //   0,
+          //   baseTime + totalPlayTime - 0.001
+          // );
         }
 
         // 停止要求をチェックするため再生時間分Slepp
